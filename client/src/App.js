e3aa5f094661300564ed7cdba1e4695f367a2529
MohcineSafi <safi.mouhcine02@gmail.com>
2024-03-26 16:08:44 +0000

    Feat:Routing with react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,21 @@
 import "./App.css";
+import { Routes, Route } from "./Routes";
+import Header from "./Header";
+import Post from "./Post";
+import Layout from "./Layout";
+import IndexPage from "./IndexPage";
+import LoginPage from "./pages/LoginPage";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<IndexPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<div>register</div>} />
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
