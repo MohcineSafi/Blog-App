fbc222b3cb797ac2b37350eb0267981776c1677f
MohcineSafi <safi.mouhcine02@gmail.com>
2024-03-28 01:13:04 +0000

    Feat:Update App with UserContextProvider

--- client/src/App.js
+++ client/src/App.js
@@ -4,18 +4,21 @@ import Header from "./Header";
 import Post from "./Post";
 import Layout from "./Layout";
 import IndexPage from "./IndexPage";
+import { UserContextProvider } from "./UserContext";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<IndexPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-      </Route>
-    </Routes>
+    <UserContextProvider>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<IndexPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+        </Route>
+      </Routes>
+    </UserContextProvider>
   );
 }
 
