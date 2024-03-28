a1be863c54aa63c14d519b3a49ee99c6023f624e
MohcineSafi <safi.mouhcine02@gmail.com>
2024-03-28 00:04:50 +0000

    Feat:Authentication (register)

---
+++ api/index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const User = require("./models/User");
+const bcrypt = require("bcryptjs");
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+
+const salt = bcrypt.genSaltSync(10);
+const secret = "asdfe45we45w345wegw345werjktjwertkj";
+
+app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+app.use(express.json());
+
+mongoose.connect;
+"mongodb+srv://mohcinsafi2002:qtHD6BhRc6jjb3nW@cluster0.pmf8eft.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"();
+
+app.post("/register", async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const userDoc = await User.create({
+      username,
+      password: bcrypt.hashSync(password, salt),
+    });
+    res.json(userDoc);
+  } catch (e) {
+    console.log(e);
+    res.status(400).json(e);
+  }
+});
+
+app.listen(4000);
